2e40dbd1216fbf9b4437e9453781880ab12cae5c
dungdev <tranquocdung1998.i3@gmail.com>
2025-01-06 13:14:04 +0700

    profile ui

--- blazor/ProfileProject/obj/Debug/net8.0/ProfileProject.AssemblyInfo.cs
+++ blazor/ProfileProject/obj/Debug/net8.0/ProfileProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProfileProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b51c23ab3474c928a675551ff8aec166cab944f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c2a2da82ca5c01da74e8e4d707b1b1255628c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProfileProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProfileProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
