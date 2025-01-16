45ded4f0285768a9ebf78acfa4ff0a0636133db7
dung <tranquocdung1998.i3@gmail.com>
2025-01-16 11:30:41 +0700

    wallet UI

---
+++ FormProject/FormProj/Models/Components/TransactionInfo.cs
@@ -0,0 +1,25 @@
+namespace FormProj.Models.Components;
+public enum TransactionType : int
+{
+    None = 0,
+    Withdraw = 1,
+    Deposit = 2
+}
+
+public class TransactionInfo
+{
+    public TransactionType TransactionType { get; set; } = TransactionType.None;
+    public string TransactionName
+    {
+        get
+        {
+            if (TransactionType == TransactionType.None) return "";
+            if (TransactionType == TransactionType.Withdraw) return "Withdraw";
+            return "Deposit";
+        }
+    }
+
+    public double Money { get; set; }
+
+    public DateTime CreateDate { get; set; } = DateTime.Now;
+}
---
+++ FormProject/FormProj/Models/Components/WalletInfoViewModel.cs
@@ -0,0 +1,7 @@
+namespace FormProj.Models.Components;
+public class WalletInfoViewModel
+{
+    public string Title { get; set; } = "Wallet";
+    public double Money { get; set; } = 0;
+
+}
